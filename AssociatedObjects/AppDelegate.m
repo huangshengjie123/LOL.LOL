537f189d8cb8a3bd434276cc7466910a631bcacd
huangshengjie <huangshengjie@hsj123.local>
2017-02-07 17:02:41 +0800

    no message

--- AssociatedObjects/AppDelegate.m
+++ AssociatedObjects/AppDelegate.m
@@ -15,6 +15,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"nihao ");
     return YES;
 }
 

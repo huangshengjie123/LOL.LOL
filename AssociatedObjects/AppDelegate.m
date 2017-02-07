c90a2f8c307dc9fa4e1e3edb8ee5d3eba17045f7
huangshengjie <huangshengjie@hsj123.local>
2017-02-07 18:56:45 +0800

    回滚bbc7

--- AssociatedObjects/AppDelegate.m
+++ AssociatedObjects/AppDelegate.m
@@ -16,7 +16,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"nihao ");
-    NSLog(@"hdjsdhj");
     return YES;
 }
 
